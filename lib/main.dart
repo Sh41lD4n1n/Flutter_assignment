a1c3eded0730666708c7e35124b44dc1007a16e9
danilXX2000 <shalagin.danil@mail.ru>
2022-02-21 02:01:21 +0300

    add

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:json_annotation/json_annotation.dart';
+import 'dart:convert';
 import 'dart:io';
 
 void main() => runApp(const MyApp());
@@ -8,13 +11,9 @@ void main() => runApp(const MyApp());
   
 }*/
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
+/*
+//Main page
+Scaffold(
       appBar: AppBar(
         title: const Text('Jokes about Chuck Norris'),
       ),
@@ -30,6 +29,236 @@ class MyApp extends StatelessWidget {
                 Text("s"),
                 Text("Here is short joke")
               ])),
-    ));
+    )
+Seccond page
+Scaffold(
+            appBar: AppBar(title: const Text('Test')),
+            body: ListView.builder(
+              itemCount: 3,
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                  child: Text(categories[index]),
+                  color: Color.fromARGB(136, 89, 89, 204),
+                  alignment: Alignment.center,
+                );
+              },
+            ))
+
+*/
+
+class JokeProvider {
+  List CATEGORIES = [];
+  String current_category = "random";
+  final String text = "";
+  JokeProvider() {
+    set_categories();
+  }
+  Future<void> set_categories() async {
+    var resp = await http
+        .get(Uri.parse('https://api.chucknorris.io/jokes/categories'));
+
+    var resp1 =
+        await http.get(Uri.parse('https://api.chucknorris.io/jokes/random'));
+
+    CATEGORIES = jsonDecode(resp.body);
+  }
+
+  Future<Widget> getJoke() async {
+    var resp =
+        await http.get(Uri.parse('https://api.chucknorris.io/jokes/random'));
+
+    return Text(jsonDecode(resp.body)["value"]);
   }
 }
+
+/*class JokeObject extends InheritedWidget {
+  const JokeObject({Key? key, required Widget child})
+      : super(key: key, child: child);
+  //final JokeProvider jokeprovifer;
+  final String text = "String";
+  final List<String> categories = const ["stre"];
+  static JokeObject of(BuildContext context) =>
+      context.dependOnInheritedWidgetOfExactType<JokeObject>()!;
+
+  @override
+  bool updateShouldNotify(covariant JokeObject oldWidget) {
+    return text != oldWidget.text || categories != oldWidget.categories;
+  }
+}
+*/
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<String> __generateCatNames() {
+    const categories = <String>['cat1', 'cat2', 'cat3'];
+    return categories;
+  }
+
+  int state = 0;
+  JokeProvider jokeProvider = JokeProvider();
+
+  Widget getCategoryPage() {
+    return Scaffold(
+        appBar: AppBar(title: const Text('Test')),
+        body: ListView.builder(
+          itemCount: 3,
+          itemBuilder: (BuildContext context, int index) {
+            return Container(
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  padding: const EdgeInsets.all(16.0),
+                  primary: Colors.white,
+                  textStyle: const TextStyle(fontSize: 20),
+                ),
+                onPressed: changeState,
+                child: Text(__generateCatNames()[index]),
+              ),
+              color: const Color.fromARGB(136, 89, 89, 204),
+              alignment: Alignment.center,
+            );
+          },
+        ));
+  }
+
+  Widget getJokePage(BuildContext context) {
+    return Scaffold(
+        body: Padding(
+            padding: EdgeInsets.all(20.0),
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                mainAxisSize: MainAxisSize.max,
+                children: const [
+                  //Image(
+                  //  image: Image.file('./random-grid.jpg'),
+                  //),
+                  Text("s"),
+                  FutureBuilder(builder: jokeProvider.getJoke())
+                ])),
+        floatingActionButton: FloatingActionButton.extended(
+          label: const Text('Categories'),
+          icon: const Icon(Icons.abc_outlined),
+          onPressed: changeState,
+          focusColor: const Color.fromARGB(184, 95, 97, 192),
+          backgroundColor: const Color.fromARGB(184, 60, 61, 144),
+        ));
+  }
+
+  void changeState() {
+    setState(() {
+      state = (state + 1) % 2;
+    });
+  }
+
+  Widget getPage(BuildContext context) {
+    switch (state) {
+      case 0:
+        return getJokePage(context);
+      case 1:
+        return getCategoryPage();
+    }
+    throw Exception('Page doesnt exist');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Jokes about Chuck Norris'),
+        ),
+        body: getPage(context));
+  }
+}
+/*class JokePage extends StatelessWidget {
+  const JokePage({Key? key}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Padding(
+            padding: EdgeInsets.all(20.0),
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                mainAxisSize: MainAxisSize.max,
+                children: const [
+                  //Image(
+                  //  image: Image.file('./random-grid.jpg'),
+                  //),
+                  Text("s"),
+                  Text("Here is short joke")
+                ])),
+        floatingActionButton: FloatingActionButton.extended(
+          label: const Text('Categories'),
+          icon: const Icon(Icons.abc_outlined),
+          onPressed: onPress,
+          focusColor: const Color.fromARGB(184, 95, 97, 192),
+          backgroundColor: const Color.fromARGB(184, 60, 61, 144),
+        ));
+  }
+}*/
+
+/*class CategoryPage extends StatelessWidget {
+  const CategoryPage({Key? key}) : super(key: key);
+
+  List<String> __generate_cat_names() {
+    const categories = <String>['cat1', 'cat2', 'cat3'];
+    return categories;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text('Test')),
+        body: ListView.builder(
+          itemCount: 3,
+          itemBuilder: (BuildContext context, int index) {
+            return Container(
+              child: Text(__generate_cat_names()[index]),
+              color: Color.fromARGB(136, 89, 89, 204),
+              alignment: Alignment.center,
+            );
+          },
+        ));
+  }
+}
+*/
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: HomePage());
+  }
+}
+
+
+/*
+Scaffold(
+            appBar: AppBar(
+              title: const Text('Jokes about Chuck Norris'),
+            ),
+            body: Padding(
+                padding: EdgeInsets.all(20.0),
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    mainAxisSize: MainAxisSize.max,
+                    children: const [
+                      //Image(
+                      //  image: Image.file('./random-grid.jpg'),
+                      //),
+                      Text("s"),
+                      Text("Here is short joke")
+                    ])),
+            floatingActionButton: FloatingActionButton.extended(
+              label: const Text('Categories'),
+              icon: Icon(Icons.abc_outlined),
+              onPressed: () {},
+              focusColor: Color.fromARGB(184, 95, 97, 192),
+              backgroundColor: Color.fromARGB(184, 60, 61, 144),
+            )));
+ */
